93844a3a2cb4a7b1d7ff3b3c9104bdc91796c7c6
Javier Castro <jaca2899@gmail.com>
2020-06-09 14:09:38 -0400

    Imported `models.py` into `views.py` in the dictionary app.

--- dictionary/views.py
+++ dictionary/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+from dictionary.models import *
+
+def dictionary_entry(request):
+    query = request.GET.get('q')
+    if query:
+        L = Lexicon()
+        L.load('lexicon.json')
+        search = L.search(query)
+        if search:
+            c, d = search
+            context = {lemma: query, etymology = "Apo nevidonná originá", category: c, definition: d}
+            return render(request, '_dictionary-entry.html', context)
+        else:
+            HttpResponse("afal")
