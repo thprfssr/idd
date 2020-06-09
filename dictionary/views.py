7bee5dea41acee5a1ac7cc92ab0a25aa78ad57e3
Javier Castro <jaca2899@gmail.com>
2020-06-09 15:35:23 -0400

    Worked on the dictionary search, and made it so that it now displays the definition and category.

--- dictionary/views.py
+++ dictionary/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from dictionary.models import *
-
+"""
 def dictionary_entry(request):
     query = request.GET.get('q')
     if query:
@@ -21,3 +21,21 @@ def dictionary_entry(request):
         #else:
             #return HttpResponse("afal")
             return context
+"""
+
+def dictionary_entry(request):
+    query = request.GET.get('q')
+    if query:
+        L = Lexicon()
+        L.load('dictionary/lexicon.json')
+        matches = L.search(query)
+
+        matches_list = []
+        for lemma in sorted(matches.database.keys()):
+            category = matches.database[lemma]['category']
+            definition = matches.database[lemma]['definition']
+            matches_list.append({'lemma': lemma, 'category': category, 'definition': definition})
+
+        context = {'matches': matches_list}
+
+    return context
