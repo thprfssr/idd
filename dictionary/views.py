a0c5ce9744348a8ece6ee50a5f74fe0c1b6cf12d
Javier Castro <jaca2899@gmail.com>
2020-06-09 14:43:22 -0400

    Made it so that the search query is only displayed if it exists in the dictionary.

--- dictionary/views.py
+++ dictionary/views.py
@@ -9,14 +9,13 @@ from dictionary.models import *
 def dictionary_entry(request):
     query = request.GET.get('q')
     if query:
-        #L = Lexicon()
-        #L.load('lexicon.json')
-        #search = L.search(query)
-        if True:
+        L = Lexicon()
+        L.load('dictionary/lexicon.json')
+        search = L.search(query)
+        if search:
             #c, d = search
             etymology = "Apo nevidonná originá"
-            category = "n."
-            definition = "alfjlajfjafj ladjfj ljaldfl jajf l"
+            category, definition = search
             context = {'lemma': query, 'etymology': etymology, 'category': category, 'definition': definition}
             #return render(request, '_dictionary-entry.html', context)
         #else:
