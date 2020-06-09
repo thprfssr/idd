1fe33f736de75dd49158d4ef70dbcc05374fb941
Javier Castro <jaca2899@gmail.com>
2020-06-09 03:57:22 -0400

    I finally managed to make the search button do something!

--- pages/views.py
+++ pages/views.py
@@ -2,5 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from django.http import HttpResponse
+
 def index(request):
+
+    if request.GET.get('q'):
+        return HttpResponse("adlfj")
+
     return render(request, 'base.html')
+
+def search(request):
+    query = request.GET['q']
+    return HttpResponse("Hello World!")
